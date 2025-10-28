b656a23b8d06d95f2d61827d090b4c0437f553e0
Waleed <waleedeveloper100@gmail.com>
2025-10-28 14:44:42 +0500

    Js assignment 1 practice completed

---
+++ assignment-1/script.js
@@ -0,0 +1,133 @@
+console.log("Connected!")
+// alert("Hello from Sheryians")
+
+//If HTML is skeleton, CSS is clothes and JS is brain.
+
+//Write one example of where you've seen JS being used.
+//It is used in animations like loaders effects, mouse move effects and popups like conformation,cencelation and offers and many more things we can do with js.
+
+
+// alert("Hi")
+// prompt("Your name?")
+
+// let city = "Islamabad"
+// console.log(city)
+
+// var a = "Muhammad";
+// let b = "Waleed";
+// const c = "Code";
+// a = "Updated";
+// b = "Updated";
+// c = "Updated";
+// console.log(a);
+// console.log(b);
+// console.log(c);
+
+
+// {
+//     let role = "admin"
+// }
+// console.log(role);
+
+
+//const examples
+// const Pi = 3.14;
+// const discount = 10;
+// const baseURL = "https://google.com"
+
+
+// let name = "Waleed";
+// let age  = 20;
+// let city = "Islamabad";
+// console.log(name,age,city)
+// console.info(name,age,city)
+// console.warn(name,age,city)
+
+
+// let prmpt = prompt("user name?")
+// console.log(typeof prmpt)
+// // alert(`Welcome ${prmpt}`)
+
+// let age = prompt("Enter age:")
+// console.log(age + 5) //it give string so age will concat with 5
+
+
+// let msg = "I love Sheryians"
+// console.log(msg.includes("love"))
+// console.log(msg.slice(2,6));
+// console.log(msg.split(""))
+// console.log(msg.replace("love", "study at"));
+
+// let name = "Waleed";
+// console.log(`Hey ${name}`)
+
+
+// let a = 12, b = 1;
+// console.log(a, b);
+
+
+
+// let a = 1
+// let b = 2
+// let c = 3
+// console.log(a);
+// console.log(b);
+// console.log(c);
+
+
+
+
+// Muhammad Waleed
+//I am practicing variables
+//printing message in console.log with prompt
+//showing alert message
+
+// 5 + 10 expression
+// let x = 10 statement
+// which one gives a value immediately?
+// ans: expression gives value immediately
+
+// let y = (5 + 10) * 2
+// console.log(y)
+
+
+//expression which return a value it calculate and give value
+//statement it does not return value 
+
+// let age = 25;
+// console.log(typeof age);
+// let name = "Waleed";
+// console.log(typeof name);
+// let isStudent = true;
+// console.log(typeof isStudent)
+// let skills = ["JS","HTML"];
+// console.log(typeof skills)
+// let user = {city: "Islamabad"};
+// console.log(typeof user);
+// let x = null;
+// console.log(typeof x)
+// let y;
+// console.log(typeof y);
+// let z = Symbol("id")
+// console.log(typeof z);
+
+
+// console.log(1 / 0)
+// console.log(0 / 0)
+// console.log(Number("abc"))
+// console.log(undefined + 1)
+
+
+// let x = 5;
+// let y = x;
+// y = 10;
+// console.log(x, y);
+
+// let obj1 = {
+//     name: "Waleed",
+// }
+// let obj2 = obj1;
+// obj2.name = "Muhammad"
+// console.log(obj1.name)
+// console.log(obj2.name)
+
