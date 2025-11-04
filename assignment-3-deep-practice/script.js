83b19b2e4220b6ba51fd249e34047562906235db
Waleed <waleedeveloper100@gmail.com>
2025-11-04 10:51:10 +0500

    assignment-3 deep practice 7 questions are solved

--- assignment-3-deep-practice/script.js
+++ assignment-3-deep-practice/script.js
@@ -44,3 +44,78 @@
 //     }
 // }
 // console.log(`total count is ${count}`)
+
+
+
+
+// Ask user for password and print access status
+// Hardcoded correct password. Compare with user input.
+
+// let myPassword = prompt("Enter your password");
+// let password = "Waleed123";
+
+// if(myPassword === null){
+//     console.error("You cancelled it!")
+// }else{
+//     if(myPassword === password){
+//         console.log("Access granted 🎉")
+//     }else{
+//         console.error("Password not matched! ❌")
+//     }
+// }
+
+
+
+//LEVEL 2
+//  Allow only 3 attempts to enter correct password
+// If user gets it right early, stop. If not → “Account locked”
+
+// let password = "Wal23$s";
+// let attempts = 0;
+// let isCorrect = false;
+
+// let myPassword = prompt("Enter your password")
+// attempts++
+
+// if (myPassword === password) {
+//     isCorrect = true;
+//     console.log("Password is matched 🎉")
+// }
+
+// while (myPassword !== password) {
+//     if (attempts === 3) {
+//         console.error("Account Locked")
+//         break;
+//     }
+//     myPassword = prompt(`Enter your correct password attempt ${attempts}`)
+//     attempts++
+//     if (myPassword === password) {
+//         isCorrect = true;
+//         console.log("Password is matched 🎉")
+//     }
+
+// }
+
+
+
+// Print numbers divisible by 7 from 1 to 50
+// Use modulo % and loop.
+
+
+// for (let i = 1; i <= 50; i++) {
+//     if(i%7 === 0){
+//         console.log(i);
+//     }
+// }
+
+
+//Sum of all odd numbers from 1 to 30
+// Add only odd numbers. Print final sum.
+
+// let sum = 0;
+// for (let i = 1; i <= 30; i++) {
+//     if (i % 2 === 1) {
+//         sum += i
+//     }
+// }
+// console.log(sum);
