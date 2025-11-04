ef7448220dc4d55419358145d196cea0c515e279
Waleed <waleedeveloper100@gmail.com>
2025-11-04 10:23:27 +0500

    assignment-3 deep practice 3 questions are solved

---
+++ assignment-3-deep-practice/script.js
@@ -0,0 +1,46 @@
+//practice start
+
+//Question 1
+//Ask user’s age and check if eligible to vote
+// If age >= 18 → “Eligible”, else → “Not eligible”
+
+// let age = prompt("Enter your age");
+
+// if (age === null) {
+//     console.error("You cancelled!")
+// } else if (age.trim() === "") {
+//     console.error("You did not enter anything in input");
+// }
+// else {
+//     const numAge = Number(age)
+//     if (isNaN(numAge)) {
+//         console.error("You enter string only add number")
+//     }
+//     else if (numAge < 0) {
+//         console.error("Age must be in positive number")
+//     }
+//     else {
+//         console.log(`Your age is ${numAge}`)
+//     }
+// }
+
+
+
+// Print multiplication table of 5
+// Use loop to print 5 × 1 to 5 × 10.
+
+// for (let i = 1; i <= 10; i++) {
+//     console.log(`5 x ${i} = ${5 * i}`)
+// }
+
+
+// Count how many numbers between 1 and 15 are greater than 8
+// Loop and count conditionally.
+// let count = 0;
+// for (let i = 1; i < 15; i++) {
+//     if (i > 8) {
+//         count++
+//         console.log(i)
+//     }
+// }
+// console.log(`total count is ${count}`)
