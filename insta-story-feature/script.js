43457f22bc4826442df409920df1f5bd8dd1239c
Waleed <waleedeveloper100@gmail.com>
2025-11-14 17:13:49 +0500

    instagram story freature completed

---
+++ insta-story-feature/script.js
@@ -0,0 +1,43 @@
+
+let users = [
+    {
+        dp: "https://images.unsplash.com/photo-1515886657613-9f3515b0c78f?q=80&w=720&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        story: "https://images.unsplash.com/photo-1727107047534-99afc152baf7?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1yZWxhdGVkfDJ8fHxlbnwwfHx8fHw%3D",
+
+    },
+    {
+        dp: "https://images.unsplash.com/photo-1524504388940-b1c1722653e1?q=80&w=687&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        story: "https://images.unsplash.com/photo-1722029117947-0243547785e1?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1yZWxhdGVkfDZ8fHxlbnwwfHx8fHw%3D",
+
+    },
+    {
+        dp: "https://images.unsplash.com/photo-1529626455594-4ff0802cfb7e?q=80&w=687&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        story: "https://images.unsplash.com/photo-1651263931378-0dfc86dc70ac?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1yZWxhdGVkfDJ8fHxlbnwwfHx8fHw%3D",
+
+    },
+    {
+        dp: "https://plus.unsplash.com/premium_photo-1682095757120-c9abb908ed60?q=80&w=687&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        story: "https://plus.unsplash.com/premium_photo-1682095664848-014a0a2bfd8a?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1yZWxhdGVkfDEyfHx8ZW58MHx8fHx8",
+
+    },
+    {
+        dp: "https://images.unsplash.com/photo-1534528741775-53994a69daeb?q=80&w=764&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        story: "https://images.unsplash.com/photo-1758922584983-82ffd5720c6a?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1yZWxhdGVkfDZ8fHxlbnwwfHx8fHw%3D",
+
+    },
+]
+let storiesbox = document.querySelector(".stories")
+let viewImg = document.querySelector(".content img")
+let content = document.querySelector(".content")
+let clutter = "";
+users.forEach((user, index) => {
+    clutter += `<div class="story">
+    <img id=${index} src=${user.dp} alt="${index}-user">
+    </div>`
+})
+storiesbox.innerHTML = clutter
+
+storiesbox.addEventListener("click", function (dets) {
+    content.style.opacity = 1;
+    viewImg.src = users[dets.target.id].story
+})
